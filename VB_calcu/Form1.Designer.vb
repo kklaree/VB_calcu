2dd2f331c7d4b59a1202b8571d353d547c6991a4
PC <PC@DEGHIE>
2023-11-05 23:17:47 +0800

    Add project files.

---
+++ VB_calcu/Form1.Designer.vb
@@ -0,0 +1,405 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtDisplay = New System.Windows.Forms.TextBox()
+        Me.CmdClear = New System.Windows.Forms.Button()
+        Me.percent = New System.Windows.Forms.Button()
+        Me.CmdDel = New System.Windows.Forms.Button()
+        Me.CmdDivide = New System.Windows.Forms.Button()
+        Me.btn7 = New System.Windows.Forms.Button()
+        Me.btn8 = New System.Windows.Forms.Button()
+        Me.btn9 = New System.Windows.Forms.Button()
+        Me.CmdMultiply = New System.Windows.Forms.Button()
+        Me.btn4 = New System.Windows.Forms.Button()
+        Me.btn5 = New System.Windows.Forms.Button()
+        Me.btn6 = New System.Windows.Forms.Button()
+        Me.CmdSubtract = New System.Windows.Forms.Button()
+        Me.btn1 = New System.Windows.Forms.Button()
+        Me.btn2 = New System.Windows.Forms.Button()
+        Me.btn3 = New System.Windows.Forms.Button()
+        Me.CmdAdd = New System.Windows.Forms.Button()
+        Me.btn00 = New System.Windows.Forms.Button()
+        Me.btn0 = New System.Windows.Forms.Button()
+        Me.btnDec = New System.Windows.Forms.Button()
+        Me.CmdAns = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.display = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtDisplay
+        '
+        Me.txtDisplay.BackColor = System.Drawing.Color.Black
+        Me.txtDisplay.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.txtDisplay.Font = New System.Drawing.Font("Microsoft Sans Serif", 27.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtDisplay.ForeColor = System.Drawing.Color.White
+        Me.txtDisplay.Location = New System.Drawing.Point(12, 87)
+        Me.txtDisplay.Name = "txtDisplay"
+        Me.txtDisplay.Size = New System.Drawing.Size(264, 42)
+        Me.txtDisplay.TabIndex = 0
+        Me.txtDisplay.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'CmdClear
+        '
+        Me.CmdClear.BackColor = System.Drawing.Color.Black
+        Me.CmdClear.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.CmdClear.Font = New System.Drawing.Font("Ebrima", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CmdClear.ForeColor = System.Drawing.Color.White
+        Me.CmdClear.Location = New System.Drawing.Point(12, 146)
+        Me.CmdClear.Name = "CmdClear"
+        Me.CmdClear.Size = New System.Drawing.Size(63, 51)
+        Me.CmdClear.TabIndex = 1
+        Me.CmdClear.Text = "C"
+        Me.CmdClear.UseVisualStyleBackColor = False
+        '
+        'percent
+        '
+        Me.percent.BackColor = System.Drawing.Color.Black
+        Me.percent.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.percent.Font = New System.Drawing.Font("Lucida Console", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.percent.ForeColor = System.Drawing.Color.White
+        Me.percent.Location = New System.Drawing.Point(81, 150)
+        Me.percent.Name = "percent"
+        Me.percent.Size = New System.Drawing.Size(63, 51)
+        Me.percent.TabIndex = 2
+        Me.percent.Text = "%"
+        Me.percent.UseVisualStyleBackColor = False
+        '
+        'CmdDel
+        '
+        Me.CmdDel.BackColor = System.Drawing.Color.Black
+        Me.CmdDel.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.CmdDel.Font = New System.Drawing.Font("Wingdings", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(2, Byte))
+        Me.CmdDel.ForeColor = System.Drawing.Color.White
+        Me.CmdDel.Location = New System.Drawing.Point(150, 149)
+        Me.CmdDel.Name = "CmdDel"
+        Me.CmdDel.Size = New System.Drawing.Size(63, 51)
+        Me.CmdDel.TabIndex = 3
+        Me.CmdDel.Text = "x"
+        Me.CmdDel.UseVisualStyleBackColor = False
+        '
+        'CmdDivide
+        '
+        Me.CmdDivide.BackColor = System.Drawing.Color.Black
+        Me.CmdDivide.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.CmdDivide.Font = New System.Drawing.Font("Lucida Console", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CmdDivide.ForeColor = System.Drawing.Color.Aqua
+        Me.CmdDivide.Location = New System.Drawing.Point(213, 147)
+        Me.CmdDivide.Name = "CmdDivide"
+        Me.CmdDivide.Size = New System.Drawing.Size(63, 51)
+        Me.CmdDivide.TabIndex = 4
+        Me.CmdDivide.Text = "÷"
+        Me.CmdDivide.UseVisualStyleBackColor = False
+        '
+        'btn7
+        '
+        Me.btn7.BackColor = System.Drawing.Color.Black
+        Me.btn7.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btn7.Font = New System.Drawing.Font("Ebrima", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn7.ForeColor = System.Drawing.Color.White
+        Me.btn7.Location = New System.Drawing.Point(12, 200)
+        Me.btn7.Name = "btn7"
+        Me.btn7.Size = New System.Drawing.Size(63, 51)
+        Me.btn7.TabIndex = 5
+        Me.btn7.Text = "7"
+        Me.btn7.UseVisualStyleBackColor = False
+        '
+        'btn8
+        '
+        Me.btn8.BackColor = System.Drawing.Color.Black
+        Me.btn8.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btn8.Font = New System.Drawing.Font("Ebrima", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn8.ForeColor = System.Drawing.Color.White
+        Me.btn8.Location = New System.Drawing.Point(79, 200)
+        Me.btn8.Name = "btn8"
+        Me.btn8.Size = New System.Drawing.Size(63, 51)
+        Me.btn8.TabIndex = 6
+        Me.btn8.Text = "8"
+        Me.btn8.UseVisualStyleBackColor = False
+        '
+        'btn9
+        '
+        Me.btn9.BackColor = System.Drawing.Color.Black
+        Me.btn9.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btn9.Font = New System.Drawing.Font("Ebrima", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn9.ForeColor = System.Drawing.Color.White
+        Me.btn9.Location = New System.Drawing.Point(146, 200)
+        Me.btn9.Name = "btn9"
+        Me.btn9.Size = New System.Drawing.Size(63, 51)
+        Me.btn9.TabIndex = 7
+        Me.btn9.Text = "9"
+        Me.btn9.UseVisualStyleBackColor = False
+        '
+        'CmdMultiply
+        '
+        Me.CmdMultiply.BackColor = System.Drawing.Color.Black
+        Me.CmdMultiply.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.CmdMultiply.Font = New System.Drawing.Font("Lucida Console", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CmdMultiply.ForeColor = System.Drawing.Color.Aqua
+        Me.CmdMultiply.Location = New System.Drawing.Point(213, 200)
+        Me.CmdMultiply.Name = "CmdMultiply"
+        Me.CmdMultiply.Size = New System.Drawing.Size(63, 51)
+        Me.CmdMultiply.TabIndex = 8
+        Me.CmdMultiply.Text = "×"
+        Me.CmdMultiply.UseVisualStyleBackColor = False
+        '
+        'btn4
+        '
+        Me.btn4.BackColor = System.Drawing.Color.Black
+        Me.btn4.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btn4.Font = New System.Drawing.Font("Ebrima", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn4.ForeColor = System.Drawing.Color.White
+        Me.btn4.Location = New System.Drawing.Point(12, 254)
+        Me.btn4.Name = "btn4"
+        Me.btn4.Size = New System.Drawing.Size(63, 52)
+        Me.btn4.TabIndex = 9
+        Me.btn4.Text = "4"
+        Me.btn4.UseVisualStyleBackColor = False
+        '
+        'btn5
+        '
+        Me.btn5.BackColor = System.Drawing.Color.Black
+        Me.btn5.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btn5.Font = New System.Drawing.Font("Ebrima", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn5.ForeColor = System.Drawing.Color.White
+        Me.btn5.Location = New System.Drawing.Point(79, 254)
+        Me.btn5.Name = "btn5"
+        Me.btn5.Size = New System.Drawing.Size(63, 52)
+        Me.btn5.TabIndex = 10
+        Me.btn5.Text = "5"
+        Me.btn5.UseVisualStyleBackColor = False
+        '
+        'btn6
+        '
+        Me.btn6.BackColor = System.Drawing.Color.Black
+        Me.btn6.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btn6.Font = New System.Drawing.Font("Ebrima", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn6.ForeColor = System.Drawing.Color.White
+        Me.btn6.Location = New System.Drawing.Point(146, 254)
+        Me.btn6.Name = "btn6"
+        Me.btn6.Size = New System.Drawing.Size(63, 52)
+        Me.btn6.TabIndex = 11
+        Me.btn6.Text = "6"
+        Me.btn6.UseVisualStyleBackColor = False
+        '
+        'CmdSubtract
+        '
+        Me.CmdSubtract.BackColor = System.Drawing.Color.Black
+        Me.CmdSubtract.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.CmdSubtract.Font = New System.Drawing.Font("Lucida Console", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CmdSubtract.ForeColor = System.Drawing.Color.Aqua
+        Me.CmdSubtract.Location = New System.Drawing.Point(213, 254)
+        Me.CmdSubtract.Name = "CmdSubtract"
+        Me.CmdSubtract.Size = New System.Drawing.Size(63, 52)
+        Me.CmdSubtract.TabIndex = 12
+        Me.CmdSubtract.Text = "-"
+        Me.CmdSubtract.UseVisualStyleBackColor = False
+        '
+        'btn1
+        '
+        Me.btn1.BackColor = System.Drawing.Color.Black
+        Me.btn1.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btn1.Font = New System.Drawing.Font("Ebrima", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn1.ForeColor = System.Drawing.Color.White
+        Me.btn1.Location = New System.Drawing.Point(12, 309)
+        Me.btn1.Name = "btn1"
+        Me.btn1.Size = New System.Drawing.Size(63, 52)
+        Me.btn1.TabIndex = 13
+        Me.btn1.Text = "1"
+        Me.btn1.UseVisualStyleBackColor = False
+        '
+        'btn2
+        '
+        Me.btn2.BackColor = System.Drawing.Color.Black
+        Me.btn2.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btn2.Font = New System.Drawing.Font("Ebrima", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn2.ForeColor = System.Drawing.Color.White
+        Me.btn2.Location = New System.Drawing.Point(79, 309)
+        Me.btn2.Name = "btn2"
+        Me.btn2.Size = New System.Drawing.Size(63, 52)
+        Me.btn2.TabIndex = 14
+        Me.btn2.Text = "2"
+        Me.btn2.UseVisualStyleBackColor = False
+        '
+        'btn3
+        '
+        Me.btn3.BackColor = System.Drawing.Color.Black
+        Me.btn3.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btn3.Font = New System.Drawing.Font("Ebrima", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn3.ForeColor = System.Drawing.Color.White
+        Me.btn3.Location = New System.Drawing.Point(146, 309)
+        Me.btn3.Name = "btn3"
+        Me.btn3.Size = New System.Drawing.Size(63, 52)
+        Me.btn3.TabIndex = 15
+        Me.btn3.Text = "3"
+        Me.btn3.UseVisualStyleBackColor = False
+        '
+        'CmdAdd
+        '
+        Me.CmdAdd.BackColor = System.Drawing.Color.Black
+        Me.CmdAdd.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.CmdAdd.Font = New System.Drawing.Font("Lucida Console", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CmdAdd.ForeColor = System.Drawing.Color.Aqua
+        Me.CmdAdd.Location = New System.Drawing.Point(213, 309)
+        Me.CmdAdd.Name = "CmdAdd"
+        Me.CmdAdd.Size = New System.Drawing.Size(63, 52)
+        Me.CmdAdd.TabIndex = 16
+        Me.CmdAdd.Text = "+"
+        Me.CmdAdd.UseVisualStyleBackColor = False
+        '
+        'btn00
+        '
+        Me.btn00.BackColor = System.Drawing.Color.Black
+        Me.btn00.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btn00.Font = New System.Drawing.Font("Ebrima", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn00.ForeColor = System.Drawing.Color.White
+        Me.btn00.Location = New System.Drawing.Point(12, 364)
+        Me.btn00.Name = "btn00"
+        Me.btn00.Size = New System.Drawing.Size(63, 52)
+        Me.btn00.TabIndex = 17
+        Me.btn00.Text = "00"
+        Me.btn00.UseVisualStyleBackColor = False
+        '
+        'btn0
+        '
+        Me.btn0.BackColor = System.Drawing.Color.Black
+        Me.btn0.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btn0.Font = New System.Drawing.Font("Ebrima", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn0.ForeColor = System.Drawing.Color.White
+        Me.btn0.Location = New System.Drawing.Point(79, 364)
+        Me.btn0.Name = "btn0"
+        Me.btn0.Size = New System.Drawing.Size(63, 52)
+        Me.btn0.TabIndex = 18
+        Me.btn0.Text = "0"
+        Me.btn0.UseVisualStyleBackColor = False
+        '
+        'btnDec
+        '
+        Me.btnDec.BackColor = System.Drawing.Color.Black
+        Me.btnDec.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnDec.Font = New System.Drawing.Font("Ebrima", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnDec.ForeColor = System.Drawing.Color.White
+        Me.btnDec.Location = New System.Drawing.Point(146, 364)
+        Me.btnDec.Name = "btnDec"
+        Me.btnDec.Size = New System.Drawing.Size(63, 52)
+        Me.btnDec.TabIndex = 19
+        Me.btnDec.Text = "."
+        Me.btnDec.UseVisualStyleBackColor = False
+        '
+        'CmdAns
+        '
+        Me.CmdAns.BackColor = System.Drawing.Color.Black
+        Me.CmdAns.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.CmdAns.Font = New System.Drawing.Font("Lucida Console", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CmdAns.ForeColor = System.Drawing.Color.Aqua
+        Me.CmdAns.Location = New System.Drawing.Point(213, 364)
+        Me.CmdAns.Name = "CmdAns"
+        Me.CmdAns.Size = New System.Drawing.Size(63, 52)
+        Me.CmdAns.TabIndex = 20
+        Me.CmdAns.Text = "="
+        Me.CmdAns.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Aqua
+        Me.Label1.Location = New System.Drawing.Point(20, 18)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(119, 24)
+        Me.Label1.TabIndex = 21
+        Me.Label1.Text = "☰  Standard"
+        '
+        'display
+        '
+        Me.display.AutoSize = True
+        Me.display.BackColor = System.Drawing.Color.Black
+        Me.display.Font = New System.Drawing.Font("Ebrima", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.display.ForeColor = System.Drawing.Color.DimGray
+        Me.display.Location = New System.Drawing.Point(223, 52)
+        Me.display.Name = "display"
+        Me.display.Size = New System.Drawing.Size(17, 20)
+        Me.display.TabIndex = 22
+        Me.display.Text = "  "
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(288, 429)
+        Me.Controls.Add(Me.display)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.CmdAns)
+        Me.Controls.Add(Me.btnDec)
+        Me.Controls.Add(Me.btn0)
+        Me.Controls.Add(Me.btn00)
+        Me.Controls.Add(Me.CmdAdd)
+        Me.Controls.Add(Me.btn3)
+        Me.Controls.Add(Me.btn2)
+        Me.Controls.Add(Me.btn1)
+        Me.Controls.Add(Me.CmdSubtract)
+        Me.Controls.Add(Me.btn6)
+        Me.Controls.Add(Me.btn5)
+        Me.Controls.Add(Me.btn4)
+        Me.Controls.Add(Me.CmdMultiply)
+        Me.Controls.Add(Me.btn9)
+        Me.Controls.Add(Me.btn8)
+        Me.Controls.Add(Me.btn7)
+        Me.Controls.Add(Me.CmdDivide)
+        Me.Controls.Add(Me.CmdDel)
+        Me.Controls.Add(Me.percent)
+        Me.Controls.Add(Me.CmdClear)
+        Me.Controls.Add(Me.txtDisplay)
+        Me.ForeColor = System.Drawing.Color.Black
+        Me.Name = "Form1"
+        Me.Text = "Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtDisplay As TextBox
+    Friend WithEvents CmdClear As Button
+    Friend WithEvents percent As Button
+    Friend WithEvents CmdDel As Button
+    Friend WithEvents CmdDivide As Button
+    Friend WithEvents btn7 As Button
+    Friend WithEvents btn8 As Button
+    Friend WithEvents btn9 As Button
+    Friend WithEvents CmdMultiply As Button
+    Friend WithEvents btn4 As Button
+    Friend WithEvents btn5 As Button
+    Friend WithEvents btn6 As Button
+    Friend WithEvents CmdSubtract As Button
+    Friend WithEvents btn1 As Button
+    Friend WithEvents btn2 As Button
+    Friend WithEvents btn3 As Button
+    Friend WithEvents CmdAdd As Button
+    Friend WithEvents btn00 As Button
+    Friend WithEvents btn0 As Button
+    Friend WithEvents btnDec As Button
+    Friend WithEvents CmdAns As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents display As Label
+End Class
---
+++ VB_calcu/Form1.vb
@@ -0,0 +1,126 @@
+﻿Imports System.Linq.Expressions
+
+Public Class Form1
+
+    Dim num1 As Double
+    Dim num2 As Double
+    Dim result As Double
+    Dim operation As String
+    Dim operator_selector As Boolean = False
+
+    Private Sub btn1_Click(sender As Object, e As EventArgs) Handles btn1.Click
+        Dim b As Button = sender
+        txtDisplay.Text = txtDisplay.Text + "1"
+    End Sub
+
+    Private Sub btn2_Click(sender As Object, e As EventArgs) Handles btn2.Click
+        Dim b As Button = sender
+        txtDisplay.Text = txtDisplay.Text + "2"
+    End Sub
+
+    Private Sub btn3_Click(sender As Object, e As EventArgs) Handles btn3.Click
+        Dim b As Button = sender
+        txtDisplay.Text = txtDisplay.Text + "3"
+    End Sub
+
+    Private Sub btn4_Click(sender As Object, e As EventArgs) Handles btn4.Click
+        Dim b As Button = sender
+        txtDisplay.Text = txtDisplay.Text + "4"
+    End Sub
+
+    Private Sub btn5_Click(sender As Object, e As EventArgs) Handles btn5.Click
+        Dim b As Button = sender
+        txtDisplay.Text = txtDisplay.Text + "5"
+    End Sub
+
+    Private Sub btn6_Click(sender As Object, e As EventArgs) Handles btn6.Click
+        Dim b As Button = sender
+        txtDisplay.Text = txtDisplay.Text + "6"
+    End Sub
+
+    Private Sub btn7_Click(sender As Object, e As EventArgs) Handles btn7.Click
+        Dim b As Button = sender
+        txtDisplay.Text = txtDisplay.Text + "7"
+    End Sub
+
+    Private Sub btn8_Click(sender As Object, e As EventArgs) Handles btn8.Click
+        Dim b As Button = sender
+        txtDisplay.Text = txtDisplay.Text + "8"
+    End Sub
+
+    Private Sub btn9_Click(sender As Object, e As EventArgs) Handles btn9.Click
+        Dim b As Button = sender
+        txtDisplay.Text = txtDisplay.Text + "9"
+    End Sub
+
+    Private Sub btn0_Click(sender As Object, e As EventArgs) Handles btn0.Click
+        Dim b As Button = sender
+        txtDisplay.Text = txtDisplay.Text + "0"
+    End Sub
+
+    Private Sub btn00_Click(sender As Object, e As EventArgs) Handles btn00.Click
+        Dim b As Button = sender
+        txtDisplay.Text = txtDisplay.Text + "00"
+    End Sub
+    Private Sub btnDec_Click(sender As Object, e As EventArgs) Handles btnDec.Click
+        Dim b As Button = sender
+        txtDisplay.Text = txtDisplay.Text + "."
+    End Sub
+
+    Private Sub CmdClear_Click(sender As Object, e As EventArgs) Handles CmdClear.Click
+        txtDisplay.Clear()
+    End Sub
+    Private Sub CmdDel_Click(sender As Object, e As EventArgs) Handles CmdDel.Click
+        If txtDisplay.Text.Length > 0 Then
+            txtDisplay.Text = txtDisplay.Text.Remove(txtDisplay.Text.Length - 1, 1)
+        End If
+    End Sub
+
+    Private Sub CmdAdd_Click(sender As Object, e As EventArgs) Handles CmdAdd.Click
+        num1 = txtDisplay.Text
+        txtDisplay.Text = ""
+        operator_selector = True
+        operation = 1 ' = +
+    End Sub
+
+    Private Sub CmdSubtract_Click(sender As Object, e As EventArgs) Handles CmdSubtract.Click
+        num1 = txtDisplay.Text
+        txtDisplay.Text = ""
+        operator_selector = True
+        operation = 2 ' = -
+    End Sub
+
+    Private Sub CmdMultiply_Click(sender As Object, e As EventArgs) Handles CmdMultiply.Click
+        num1 = txtDisplay.Text
+        txtDisplay.Text = ""
+        operator_selector = True
+        operation = 3 ' = *
+    End Sub
+
+    Private Sub CmdDivide_Click(sender As Object, e As EventArgs) Handles CmdDivide.Click
+        num1 = txtDisplay.Text
+        txtDisplay.Text = ""
+        operator_selector = True
+        operation = 4 ' = /
+    End Sub
+
+    Private Sub CmdAns_Click(sender As Object, e As EventArgs) Handles CmdAns.Click
+        If operator_selector = True Then
+            num2 = txtDisplay.Text
+            If operation = 1 Then
+                txtDisplay.Text = num1 + num2
+            ElseIf operation = 2 Then
+                txtDisplay.Text = num1 - num2
+            ElseIf operation = 3 Then
+                txtDisplay.Text = num1 * num2
+            Else
+                If num2 = 0 Then
+                    txtDisplay.Text = "Error!"
+                Else
+                    txtDisplay.Text = num1 / num2
+                End If
+            End If
+            operator_selector = False
+        End If
+    End Sub
+End Class
